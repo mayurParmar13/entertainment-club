3026a8d31b1e7d85974e5137ff4fd741ed4d3a16
Mayur Parmar <mayurparmar8734@gmail.com>
2022-09-10 18:51:33 +0530

    feat: header component create

---
+++ src/components/header/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./header.css"
+
+const Header = () => {
+  return (
+    <div className='header' onClick={() => window.scroll(0,0)}> 🎬 Entertainment Hub 🎥 </div>
+  )
+}
+
+export default Header
