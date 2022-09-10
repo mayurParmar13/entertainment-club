bd6b2f4ddac6dcd9d7e790932c04b3dd3e482658
Mayur Parmar <mayurparmar8734@gmail.com>
2022-09-10 19:07:58 +0530

    feat: required component create

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import './App.css';
-import { Button } from '@mui/material';
 import Header from './components/header/header';
+import { BrowserRouter, } from 'react-router-dom';
+import { Container } from '@mui/system';
+import { Route, Routes } from 'react-router';
+import Trending from './components/Trending/Trending';
+import Movies from './components/Movies/Movies';
+import Series from './components/Series/Series';
+import Search from './components/Search/Search';
 
-// test
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Button>test</Button>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <Container>
+          <Routes>
+            <>
+            <Route index={true} element={<Trending/>}/>
+            <Route path="/movies" element={<Movies/>}/>
+            <Route path="/series" element={<Series/>}/>
+            <Route path="/search" element={<Search/>}/>
+            </>
+          </Routes>
+        </Container>
+      </div>
+    </BrowserRouter>
   );
 }
 
